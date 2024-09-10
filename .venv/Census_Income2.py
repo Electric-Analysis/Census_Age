24540befec35de96954403b8b236a069d10c3a6c
Jakob <jtilsch@pvpc.org>
2024-09-10 13:55:25 -0400

    Census Age Table - Gather census data on age from B01001 & B09001.

--- .venv/Census_Income2.py
+++ .venv/Census_Income2.py
@@ -5,6 +5,38 @@ import numpy as np
 import re
 import pyodbc
 
+'''
+This script is designed to fully automate the acquisition of data from table B19001 for town, county, and state levels 
+of geography. Additionally code is being developed to include Pioneer Valley and PVPC region which are fundamentally
+row aggregates of county level data. By running this script the user will make an API call to the census bureau in 3
+seperate instances. First it will call for the county subdivision level data, then again for the county level data,
+last it will call for the state level data (just a single row). Next the data will be converted into a format that is 
+readably by python and put into a dataframe or a table. These tables will then be merged so the county and state level
+data are in-line with county sub-division level data in one table. Following this columns are inserted into this merged
+dataframe and then data is extracted from the dataset and filled into the new columns. This is the data we want in our
+final output. Once all the data is reported and calculated in these columns that we made we then initialize an empty 
+table that will be the final container for the data we need. Once the database dataframe is made we then move all the 
+data from the columns we made in the original dataframe into the new database dataframe. This database dataframe is 
+complete with all the static information that is typical to any other table we keep on file indicating STATE, TIME_TYPE,
+TIME_VALUE, YEAR, and each table specific column. Finally now that we have all the data we need in one spot with the 
+extra data stripped away we can clean up the community names so they are less descriptive and more readable at a glance.
+Lastly now that the table is full  of accurate and readable information we can print this table to a csv file designated
+to a directory and with the year of the data request built into the name. 
+
+NOTE: if you have a file open with the same
+as a file you are trying to create it will throw an error. For example you have Census_Income_2022 open. When you try to 
+get census income for 2021 it will work but 2022 will not because its open, make sure you close it. This WILL overwrite 
+any data in existing files with the same name. Comment out the last line of code to skip writing to csv by adding a 
+hashtag to the beginning of the line this will render the line completely neutralized. 
+
+From this (live code)
+Database_df.to_csv("C:/Users/jtilsch/OneDrive - Pioneer Valley Planning Commission/Desktop/Projects/Database Design/Data/Census Income Data/Census Income " + str(year) + ".csv")
+
+To this (dead code) note the # symbol
+# Database_df.to_csv("C:/Users/jtilsch/OneDrive - Pioneer Valley Planning Commission/Desktop/Projects/Database Design/Data/Census Income Data/Census Income " + str(year) + ".csv")
+
+'''
+
 B19001_Headers = ['Less than $10,000', 'Percent Less than $10,000', '$10,000 to $14,999', 'Percent $10,000 to $14,999',
                   '$15,000 to $24,999', 'Percent $15,000 to $24,999', '$25,000 to $34,999', 'Percent $25,000 to $34,999',
                   '$35,000 to $49,999', 'Percent $35,000 to $49,999', '$50,000 to $74,999', 'Percent $50,000 to $74,999',
@@ -23,7 +55,7 @@ Database_df_Headers = ['Less than $10,000', 'Percent Less than $10,000', '$10,00
 
 dataframes = []
 
-year = 2023
+year = 2022
 year2  = year - 4
 B19001_URL = "https://api.census.gov/data/"+str(year)+"/acs/acs5?get=group(B19001)&for=county%20subdivision:*&in=state:25%20county:011,013,015"
 B19001_URL_CNTY = "https://api.census.gov/data/"+str(year)+"/acs/acs5?get=group(B19001)&for=county:011,013,015&in=state:25"
@@ -53,16 +85,16 @@ merged_df = pd.concat([pd.DataFrame([pd.to_numeric(merged_df[e], errors = 'coerc
                         merged_df[['GEO_ID','NAME','state']]], axis = 1)
 
 
-print(merged_df.dtypes.to_string())
+# print(merged_df.dtypes.to_string())
 
-for i  in  merged_df['NAME']:
-    print(i)
+# for i  in  merged_df['NAME']:
+#     print(i)
 
 
 ordered_communities = []
 for i in merged_df["NAME"]:
     ordered_communities.append(i)
-print(f"ordered_communities:\n{ordered_communities}")
+# print(f"ordered_communities:\n{ordered_communities}")
 
 
 for i in B19001_Headers:
@@ -95,7 +127,7 @@ merged_df['Percent $100,000 to $149,999'] = round(merged_df.loc[:,  ["B19001_014
 merged_df['Percent $150,000 to $199,999'] = round(merged_df.loc[:,  ["B19001_016E"]].sum(axis = 1) / merged_df["B19001_001E"],4)
 merged_df['Percent $200,000 or more'] = round(merged_df.loc[:,  ["B19001_017E"]].sum(axis = 1) / merged_df["B19001_001E"],4)
 
-
+print("\n\nMerged Dataframe")
 print(merged_df.to_string())
 
 community_series = pd.Series(ordered_communities)
@@ -149,9 +181,10 @@ Database_df["COMMUNITY"] = Database_df["COMMUNITY"].str.strip()
 
 Database_df = Database_df.sort_values(by="COMMUNITY")
 
+print("\n\nDatabase Dataframe")
 print(Database_df.to_string())
 
-Database_df.to_csv("C:/Users/jtilsch/OneDrive - Pioneer Valley Planning Commission/Desktop/Projects/Database Design/Data/Census Income Data/Census Income " + str(year) + ".csv")
+# Database_df.to_csv("C:/Users/jtilsch/OneDrive - Pioneer Valley Planning Commission/Desktop/Projects/Database Design/Data/Census Income Data/Census Income " + str(year) + ".csv")
 
 
 
